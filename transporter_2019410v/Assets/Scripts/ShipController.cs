e8d0ecdf687b46520cbe1064a2e5dbcf94524bda
HandyD <dorceus.handy@gmail.com>
2021-03-08 03:16:36 -0500

    Added ShipController script to script folder and particle affects to player

---
+++ transporter_2019410v/Assets/Scripts/ShipController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    Vector3 mouseposition;
+    Vector3 changedposition;
+    Vector3 deltaposition;
+    Vector2 velocity;
+    Rigidbody2D shipbody;  
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shipbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       mouseposition.z = 0;
+       changedposition = gameObject.transform.position;
+
+       deltaposition = (mouseposition - changedposition).normalized;
+       //Debug.Log(deltaposition);
+       shipbody.velocity = deltaposition;
+    }
+}
