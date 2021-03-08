0cd661178313153c923cd58606604faa3e5e8931
erickim0223 <71943789+erickim0223@users.noreply.github.com>
2021-03-08 17:47:16 -0500

    3/8 Changes
    Added title, button, and transition to level 1

---
+++ transporter_2019410v/Assets/Scripts/GoToLEVEL1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToLEVEL1 : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("LEVEL1"); //uses level name
+    }
+}
