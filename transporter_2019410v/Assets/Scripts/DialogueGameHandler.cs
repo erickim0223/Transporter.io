38b30ab64e509e9a0f7669dfb0b56a480cfcf78f
HandyD <dorceus.handy@gmail.com>
2021-03-09 17:16:40 -0500

    Added the GamplayHandler
    I added the Gamplayer Prefab to each scene so that when a built game is run it quits the game when the user presses ESC

---
+++ transporter_2019410v/Assets/Scripts/DialogueGameHandler.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DialogueGameHandler : MonoBehaviour{
+
+        public static int playerStat;
+        //public GameObject textGameObject;
+
+        //void Start () { UpdateScore (); }
+
+        void Update(){
+                if (Input.GetKey("escape")){
+                        Application.Quit();
+                }
+        }
+
+        public void AddPlayerStat(int amount){
+                playerStat += amount;
+                Debug.Log("Current Player Stat = " + playerStat);
+        //      UpdateScore ();
+        }
+
+        //void UpdateScore () {
+        //        Text scoreTemp = textGameObject.GetComponent<Text>();
+        //        scoreTemp.text = "Score: " + score; }
+
+        public void StartGame(){
+                SceneManager.LoadScene("Scene1");
+        }
+
+        public void RestartGame(){
+                SceneManager.LoadScene("MainMenu");
+        }
+
+        public void QuitGame(){
+                Application.Quit();
+        }
+}
