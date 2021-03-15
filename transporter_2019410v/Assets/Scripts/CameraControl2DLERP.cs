ae0c825ecad94e2d37f8809489316b8c4cd2247e
HandyD <dorceus.handy@gmail.com>
2021-03-14 20:01:20 -0400

    Fixed LERP
    Camera follows player

--- transporter_2019410v/Assets/Scripts/CameraControl2DLERP.cs
+++ transporter_2019410v/Assets/Scripts/CameraControl2DLERP.cs
@@ -2,10 +2,10 @@
 
 public class CameraControl2DLERP : MonoBehaviour {
 
-	public GameObject Player;
+	public GameObject target;
 
 	void FixedUpdate () {
-		Vector2 pos = Vector2.Lerp ((Vector2)transform.position, (Vector2)Player.transform.position, Time.fixedDeltaTime);
+		Vector2 pos = Vector2.Lerp ((Vector2)transform.position, (Vector2)target.transform.position, Time.fixedDeltaTime);
 		//transform.position = new Vector3 (pos.x, pos.y, transform.position.y);
 		transform.position = new Vector3 (pos.x, pos.y, transform.position.z);
 	}
